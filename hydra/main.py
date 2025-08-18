bf27305319cc4128453797f8a3bab8fd41f470e1
malfindor <sam.burton271@gmail.com>
2025-08-18 12:17:50 -0400

    Added a function to get public IP of a given machine

--- hydra/main.py
+++ hydra/main.py
@@ -15,5 +15,17 @@ def main():
             return
     else:
         print("Hydra is up to date.")
-          
+     
+def getIP(machine):
+    return {
+            'docker': '172.25.x.97',
+            'debian': '172.25.x.20',
+            'ubuntu': '172.25.x.23',
+            'WinAD': '172.25.x.27',
+            'splunk': '172.25.x.9',
+            'ecom': '172.25.x.11',
+            'fedora': '172.25.x.39'
+        }.get(machine, "")
+
+    
 main()
